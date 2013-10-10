45b9e88ea3b852bdfeaae38c678a0184730bcdc5
Brad Gearon <bradgaron@gmail.com>
2013-10-09 21:38:13 -0500

    refactored directive, using bound props instead of exprssions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,143 +2,143 @@
 angular.module('d3App.services', [])
     .factory('graphSvc', function () {
 
-        var graph = function() { };
+      var graph = function () {};
 
-        graph.prototype.init = function (opts) {
-            if (opts && opts.element) {
+      graph.prototype.init = function (opts) {
+        if (opts && opts.element) {
+          this.graphImpl = new Rickshaw.Graph({
+            element: opts.element,
+            width: opts.width,
+            height: opts.height,
+            renderer: 'area',
+            stroke: true,
+            preserve: true,
+            series: opts.data
+          });
 
-                this.graphImpl = new Rickshaw.Graph({
-                    element: opts.element.get(0),
-                    width: opts.element.width(),
-                    height: opts.element.height(),
-                    renderer: 'area',
-                    stroke: true,
-                    preserve: true,
-                    series: opts.data
-                });
+          var ticksTreatment = 'glow';
 
-                var ticksTreatment = 'glow';
+          var xAxis = new Rickshaw.Graph.Axis.Time({
+            graph: this.graphImpl,
+            ticksTreatment: ticksTreatment,
+            timeFixture: new Rickshaw.Fixtures.Time.Local()
+          });
 
-                var xAxis = new Rickshaw.Graph.Axis.Time({
-                    graph: this.graphImpl,
-                    ticksTreatment: ticksTreatment,
-                    timeFixture: new Rickshaw.Fixtures.Time.Local()
-                });
+          var yAxis = new Rickshaw.Graph.Axis.Y({
+            graph: this.graphImpl,
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+            ticksTreatment: ticksTreatment
+          });
 
-                var yAxis = new Rickshaw.Graph.Axis.Y({
-                    graph: this.graphImpl,
-                    tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-                    ticksTreatment: ticksTreatment
-                });
+          this.graphImpl.render();
+          xAxis.render();
+          yAxis.render();
+        }
+        return this;
+      };
 
-                this.graphImpl.render();
-                xAxis.render();
-                yAxis.render();
+      graph.prototype.update = function () {
+        this.graphImpl.update();
+      };
+
+      graph.prototype.setSeries = function (data) {
+        if (data) {
+          // just replacing data with current
+          for (var i = 0; i < data.length; i++) {
+            if (this.graphImpl.series[i]) {
+              this.graphImpl.series[i] = data[i];
             }
-            return this;
-        };
+          }
+        }
+      };
 
-        graph.prototype.update = function () {
-            this.graphImpl.update();
-            return this;
-        };
-
-        graph.prototype.setSeries = function (data) {
-            if (data) {
-                // just replacing data with current
-                for (var i = 0; i < data.length; i++) {
-                    if (this.graphImpl.series[i]) {
-                        this.graphImpl.series[i] = data[i];
-                    }
-                }
-            }
-            return this;
-        };
-
+      graph.prototype.$get = function () {
         return new graph();
+      };
+
+      return graph;
     });
 
 
 angular.module('d3App.directives', ['ng', 'd3App.services'])
     .directive('graph', function (graphSvc) {
-        return {
-            // scope -- not a new one.  but appending these attributes (@)
-            scope: {
-                data: '@graphData',
-                domain: '@graphDomain',
-                type: '@graphType'
-            },
-            link: function (scope, element, attrs) {
-                var pointType = '';
-                var syncObj = '?';
+      return {
+        scope: {
+          data: '=graphData',
+          domain: '=graphDomain',
+          type: '=graphType'
+        },
+        link: function (scope, element, attrs) {
+          
 
-                var graph;
+          var pointType = '';
+          var syncObj = '?';
 
-                // this isnt used
-                var onRedraw = function (graph) {
-                    scope.domain = graph.x.domain();
-                    scope.panMode = graph.panMode;
-                    scope.$apply();
-                };
+          // this isnt used
+          var onRedraw = function (graph) {
+            scope.domain = graph.x.domain();
+            scope.panMode = graph.panMode;
+            scope.$apply();
+          };
 
-                // this isnt used
-                var onPointSelect = function (graph, point) {
-                    scope.selectedPoint = {
-                        load: point.y
-                    };
-                    scope.$apply();
-                };
+          // this isnt used
+          var onPointSelect = function (graph, point) {
+            scope.selectedPoint = {
+              load: point.y
+            };
+            scope.$apply();
+          };
+          
+          // watch the data...
+          scope.$watch('data', function (changed) {
+            if (!changed) return;
 
-                // watch the data...
-                scope.$watch('data', function (changed) {
-                    if (!changed) return;
-
-                    var value = scope.$eval(changed);
-
-                    if (!graph) {
-                        graph = graphSvc.init({
-                            element: element,
-                            data: value
-                        });
-                    }
-
-                    graph.setSeries(value);
-                    graph.update();
-
+            if (!scope.graph) {
+              scope.graph = new graphSvc().init({
+                element: element.get(0),
+                width: element.width(),
+                height: element.height(),
+                data: changed
                 });
-
-                // this isnt used
-                scope.$watch('type', function (value) {
-                    switch (value) {
-                        case 'load':
-                            break;
-                        case 'time':
-                            break;
-                        default:
-                            break;
-                    }
-                });
-
-                // watch attribute graph-domain on directive
-                // this isnt used
-                scope.$watch('domain', function (value) {
-                    if (value) {
-                        graph.panMode = scope.panMode;
-                        graph.x.domain(value);
-                    }
-                });
-
-
             }
+
+            scope.graph.setSeries(changed);
+            scope.graph.update();
+
+          });
+
+          // this isnt used
+          scope.$watch('type', function (value) {
+            switch (value) {
+              case 'load':
+                break;
+              case 'time':
+                break;
+              default:
+                break;
+            }
+          });
+
+          // watch attribute graph-domain on directive
+          // this isnt used
+          scope.$watch('domain', function (value) {
+            if (value) {
+              graph.panMode = scope.panMode;
+              graph.x.domain(value);
+            }
+          });
+
+
         }
+      }
     })
 ;
 
 
 angular.module('d3App', ['d3App.directives'])
     .config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-            .otherwise({ redirectTo: '/' });
+      $locationProvider.html5Mode(true);
+      $routeProvider
+          .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
+          .otherwise({ redirectTo: '/' });
     });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,70 +2,69 @@
 angular.module('d3App')
     .controller('MainCtrl', function ($scope) {
 
-        // setup junk. data
-        var palette = new Rickshaw.Color.Palette({ scheme: 'classic9' });
-        var seriesData = [
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            []
-        ];
-        var series = ['Moscow', 'Shanghai', 'Amsterdam', 'Paris', 'Tokyo', 'London', 'New York'];
+      // setup junk. data
+      var palette = new Rickshaw.Color.Palette({ scheme: 'classic9' });
+      var colors = [];
+      var seriesData = [];
+      var series = ['Moscow', 'Shanghai', 'Amsterdam', 'Paris', 'Tokyo', 'London', 'New York'];
 
-        // random continual data
-        var random = new Rickshaw.Fixtures.RandomData(150);
-        for (var i = 0; i < 150; i++) {
-            random.addData(seriesData);
+      for (var s = 0; s < series.length; s++) {
+        colors.push(palette.color());
+        seriesData.push([]);
+      }
+
+      // random continual data
+      var random = new Rickshaw.Fixtures.RandomData(150);
+      for (var i = 0; i < 150; i++) {
+        random.addData(seriesData);
+      }
+
+      // add data
+      var updateSeries = function (data) {
+        data = [] || data;
+        for (i = 0; i < series.length; i++) {
+          data.push({
+            color: colors[i],
+            data: seriesData[i],
+            name: series[i]
+          });
         }
+        return data;
+      };
 
-        // add data
-        var updateSeries = function (data) {
-            data = [] || data;
-            for (i = 0; i < series.length; i++) {
-                data.push({
-                    color: palette.color(),
-                    data: seriesData[i],
-                    name: series[i]
-                });
-            }
-            return data;
-        };
+      $scope.doSomething = function () {
+        console.log(arguments);
+        console.log(this);
+      };
 
-        $scope.doSomething = function () {
-            console.log(arguments);
-            console.log(this);
-        };
+      var intervalId = 0;
 
+      var addData = function () {
+        console.log('adding data');
 
-        var intervalId = 0;
+        random.removeData(seriesData);
+        random.addData(seriesData);
 
-
-        // note this will rebind when config.speed changes (rebind the interval)
-        $scope.$watch('speed', function (value) {
-
-            $scope.speed = value || 30000;
-            if (intervalId) clearInterval(intervalId);
-
-            intervalId = setInterval(function () {
-                console.log('adding data');
-
-                random.removeData(seriesData);
-                random.addData(seriesData);
-                // THIS is not in angular scope
-                $scope.data = updateSeries();
-
-                if ($scope.apply) {
-                    $scope.$apply();
-                    console.log('apply called');
-                }
-            }, value);
-        });
-
+        // THIS does not execute in angular
         $scope.data = updateSeries();
+
+        if ($scope.apply) {
+          $scope.$apply();
+          console.log('apply called');
+        }
+      };
+
+      // note this will rebind when config.speed changes (rebind the interval)
+      $scope.$watch('speed', function (value) {
+        if (intervalId) clearInterval(intervalId);
+
+        $scope.speed = value || 3000;
+        intervalId = setInterval(addData, $scope.speed);
+      });
+
+
+      $scope.data = updateSeries();
+      $scope.apply = true;
+      $scope.speed = 3000;
 
     });
